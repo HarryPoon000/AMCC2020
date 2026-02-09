e6b21138c2bb9641c7ee4d488927fe3e7a4e34f2
POON <thpoonaa@ust.hk>
2026-02-09 16:07:18 +0800

    random colors

--- randomness-0/sketch.js
+++ randomness-0/sketch.js
@@ -3,24 +3,25 @@ let radius = 200;
 function setup() {
     createCanvas(500, 500);
     background(255);
-    stroke(0);          // black
     strokeWeight(2);    // width
-    noLoop(); // disables refresh
+    // noLoop(); // disables refresh
 }
 
 function draw() {
+    background(255, 15);
     for (let i = 0; i < 600; i++) {
         // choose a random point inside the circle
         let angle = random(TWO_PI);
         let r = radius * sqrt(random()); // more even distribution
         let x = width / 2 + r * cos(angle);
         let y = height / 2 + r * sin(angle);
-        let len = random(-15, 15) * 100/abs(r+20)
-        if (random(0, 1) > 0.5) {
+        let len = random(-15, 15)
+        if (random() > 0.5) {
             line(x, y, x + len, y)
         } else {
             line(x, y, x, y + len)
         }
+        stroke(random(255), random(255), random(255))
         point(x, y);
     }
 }
