e6381bac285b606cbbab620ce4926ebe89a8afb3
POON <thpoonaa@ust.hk>
2026-02-09 16:33:44 +0800

    multiple bouncing balls

--- lerp-0/sketch.js
+++ lerp-0/sketch.js
@@ -1,20 +1,38 @@
-let x = 0, y = 0;
-let targetX = 0, targetY = 0;
+let circles = []
 
 function setup() {
     createCanvas(400, 400);
+
+    for (i = 0; i < 15; i++) {
+        circles.push(new BouncingCircle())
+    }
 }
 
 function draw() {
     background(220, 90);
 
-    if (dist(x, y, targetX, targetY) < 2) {
-        targetX = random(width)
-        targetY = random(height)
-    }
-
-    x = lerp(x, targetX, 0.1);
-    y = lerp(y, targetY, 0.1);
-    circle(x, y, 20);
+    circles.forEach(c => {
+        c.show()
+    })
 }
 
+class BouncingCircle {
+
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+        this.targetX = x;
+        this.targetY = y;
+    }
+
+    show() {
+        if (dist(this.x, this.y, this.targetX, this.targetY) < 1) {
+            this.targetX = random(width)
+            this.targetY = random(height)
+        }
+
+        this.x = lerp(this.x, this.targetX, 0.1);
+        this.y = lerp(this.y, this.targetY, 0.1);
+        circle(this.x, this.y, 20);
+    }
+}
