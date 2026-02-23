48dcc734ab75d2979a9d30f72a9c3d27cb84c915
HarryPoon000 <poontszhang@gmail.com>
2026-02-23 15:57:02 +0800

    visualise 2d noise

--- noise-2d-vis/sketch.js
+++ noise-2d-vis/sketch.js
@@ -1,7 +1,19 @@
+let tx = 0, ty = 0;
+
 function setup() {
   createCanvas(400, 400);
+  noLoop()
 }
 
 function draw() {
-  background(220);
+  background(0);
+  for (let y = 0; y < height; y++) {
+    tx = 0;
+    for (let x = 0; x < width; x++) {
+      let bright = noise(x * 0.01, y * 0.01) * 255;
+      fill(bright);
+      set(x, y, color(bright));
+    }
+  }
+  updatePixels()
 }
