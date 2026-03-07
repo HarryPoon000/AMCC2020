6c7b1b05db4a3dbeb54e2d3713c15fcf348a0d20
HarryPoon000 <poontszhang@gmail.com>
2026-03-07 14:47:53 +0800

    sound

--- p5-soundin/sketch.js
+++ p5-soundin/sketch.js
@@ -1,17 +1,43 @@
-let mic;
+let mic, fft, _miclevel;
 
 function setup() {
     createCanvas(400, 400);
-
+    fft = new p5.FFT(0.01, 256);
     mic = new p5.AudioIn();
     mic.start();
-    // mic.connect();
+    mic.connect();
 }
 
 function draw() {
-    background(220);
-
+    let spectrum = fft.analyze();
+    colorMode(HSB)
     let micLevel = mic.getLevel();
-    let size = map(micLevel, 0, 1, width / 16, width * 2);
-    ellipse(width / 2, height / 2, size);
+    _miclevel = lerp(_miclevel, micLevel, 0.1)
+    background(
+        color(micLevel * 255, 100, 100),
+        0.1
+    );
+
+
+    // let size = map(micLevel, 0, 1, width / 16, width * 2);
+    // ellipse(width / 2, height / 2, size);
+
+
+    let waveform = fft.waveform();
+
+    push()
+    noFill();
+    beginShape();
+    translate(width/2, height/2)
+    stroke(20);
+    strokeWeight(2)
+    for (let i = 0; i < waveform.length; i++) {
+        let radius = 50 + map(waveform[i], -1, 1, 0, 200);
+        let rad = map(i, 0, waveform.length-1, 0, 2 * PI)
+        let x = radius * Math.cos(rad);
+        let y = radius * Math.sin(rad);
+        vertex(x, y);
+    }
+    endShape();
+    pop();
 }
